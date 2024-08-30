150f8c2cd2eab509ae920963b27913a1878ef164
Nelson Costa <nelsonscosta@yahoo.com.br>
2024-08-30 11:01:35 -0300

    validando campos do formLogin

--- app/src/main/java/com/nsc/appdelivery/Form_login.java
+++ app/src/main/java/com/nsc/appdelivery/Form_login.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Form_login extends AppCompatActivity {
 
-    private TextView txt_criar_comta;
+    private EditText edit_email, edit_senha;
+    private Button bt_entrar;
+    private TextView txt_criar_comta, txt_mensagemErro;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +31,28 @@ public class Form_login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if(email.isEmpty() || senha.isEmpty()){
+                      txt_mensagemErro.setText("Preencha todos os campos!");
+                }else {
+
+                }
+            }
+        });
     }
 
     public void iniciarComponentes(){
         txt_criar_comta = findViewById(R.id.txt_criar_conta);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        txt_mensagemErro = findViewById(R.id.txt_mensagemErro);
     }
 }
