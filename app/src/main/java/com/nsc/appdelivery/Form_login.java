9c981644321078b8319bd046ac49138d824e0f83
Nelson Costa <nelsonscosta@yahoo.com.br>
2024-08-30 12:00:31 -0300

    Autenticando usuarios para a tela de lista de produtos

--- app/src/main/java/com/nsc/appdelivery/Form_login.java
+++ app/src/main/java/com/nsc/appdelivery/Form_login.java
@@ -1,19 +1,31 @@
 package com.nsc.appdelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class Form_login extends AppCompatActivity {
 
     private EditText edit_email, edit_senha;
     private Button bt_entrar;
     private TextView txt_criar_comta, txt_mensagemErro;
+    private ProgressBar progressBar;
 
 
 
@@ -42,17 +54,56 @@ public class Form_login extends AppCompatActivity {
                 if(email.isEmpty() || senha.isEmpty()){
                       txt_mensagemErro.setText("Preencha todos os campos!");
                 }else {
-
+                    txt_mensagemErro.setText("");
+                    AutenticarUsuario();
                 }
             }
         });
     }
 
+    public  void AutenticarUsuario() {
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            IniciarTelaProdutos();
+                        }
+                    },3000);
+                } else {
+                    String erro;
+
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e) {
+                         erro = "senha inválida";
+                    } catch (Exception e) {
+                        erro = "Erro ao cadastrar usuário!";
+                    }
+                    txt_mensagemErro.setText(erro);
+                }
+            }
+        });
+    }
+
+    public void IniciarTelaProdutos(){
+        Intent intent = new Intent(Form_login.this, Lista_Produtos.class);
+        startActivity(intent);
+        finish();
+    }
     public void iniciarComponentes(){
         txt_criar_comta = findViewById(R.id.txt_criar_conta);
         edit_email = findViewById(R.id.edit_email);
         edit_senha = findViewById(R.id.edit_senha);
         bt_entrar = findViewById(R.id.bt_entrar);
         txt_mensagemErro = findViewById(R.id.txt_mensagemErro);
+        progressBar = findViewById(R.id.progressBar);
     }
 }
