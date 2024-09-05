8ca8c226d4103acd3dd89ac1c9a608fe11576c7c
Nelson Costa <nelsonscosta@yahoo.com.br>
2024-09-05 16:38:11 -0300

    criação de menus e deslogar usuário

--- app/src/main/java/com/nsc/appdelivery/Lista_Produtos.java
+++ app/src/main/java/com/nsc/appdelivery/Lista_Produtos.java
@@ -1,8 +1,15 @@
 package com.nsc.appdelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Lista_Produtos extends AppCompatActivity {
 
@@ -10,5 +17,32 @@ public class Lista_Produtos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_produtos);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int itemID = item.getItemId();
+
+        if(itemID == R.id.perfil){
+            
+        } else if (itemID == R.id.pedidos) {
+            
+        } else if (itemID == R.id.deslogar) {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(Lista_Produtos.this, "Usuário deslogado", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Lista_Produtos.this, Form_login.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
