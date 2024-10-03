ef4e3aad01282b406c15831bd1308b86afd99773
Nelson Costa <nelsonscosta@yahoo.com.br>
2024-10-03 10:25:14 -0300

    recuperando dados do perfil usuario parte2

--- app/src/main/java/com/nsc/appdelivery/Perfil_Ususario.java
+++ app/src/main/java/com/nsc/appdelivery/Perfil_Ususario.java
@@ -10,13 +10,19 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Perfil_Ususario extends AppCompatActivity {
 
-    private CircleImageView foto_usuario;
-    private TextView nome_usuario, email_usuario;
+    private CircleImageView foto_perfil_usuario;
+    private TextView nome_perfil_usuario, email_perfil_usuario;
     private Button bt_editarPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_ususario);
+    }
+
+    public void inicarComponentes() {
+        foto_perfil_usuario = findViewById(R.id.foto_perfil_usuario);
+        nome_perfil_usuario = findViewById(R.id.nome_perfil_usuario);
+        email_perfil_usuario = findViewById(R.id.email_perfii_usuario);
     }
 }
