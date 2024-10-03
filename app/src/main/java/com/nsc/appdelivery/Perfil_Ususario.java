e0a9640397c2db399aeb0926bbe4fead63c0621e
Nelson Costa <nelsonscosta@yahoo.com.br>
2024-10-03 10:44:13 -0300

    recuperando dados do perfil usuario parte5

--- app/src/main/java/com/nsc/appdelivery/Perfil_Ususario.java
+++ app/src/main/java/com/nsc/appdelivery/Perfil_Ususario.java
@@ -1,5 +1,6 @@
 package com.nsc.appdelivery;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,7 +8,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,7 +36,15 @@ public class Perfil_Ususario extends AppCompatActivity {
         super.onStart();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        
+       String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+
+        DocumentReference documentReference = db.collection("usuarios").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                
+            }
+        });
     }
 
     public void iniciarComponentes() {
